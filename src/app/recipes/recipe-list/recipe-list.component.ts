d222a2adca6f25cb560b9677cb3df0dee2bfb224
Ankit <ankitchobdar@gmail.com>
2022-06-03 10:28:34 +0530

    Added more details to components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe','This is simply a test maybe','https://c2.staticflickr.com/6/5134/30065852941_76d1d0a157_b.jpg'),
+    new Recipe('A test recipe','This is simply a test maybe','https://c1.staticflickr.com/9/8409/30001106280_676f968089_b.jpg')
+  ];
 
   constructor() { }
 
