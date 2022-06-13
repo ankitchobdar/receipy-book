01942fa54ce93e905a54190a7bda3041faf44228
Ankit <ankitchobdar@gmail.com>
2022-06-13 14:57:33 +0530

    Before custom directives

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Output, EventEmitter } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
@@ -7,14 +8,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A test recipe','This is simply a test maybe','https://c2.staticflickr.com/6/5134/30065852941_76d1d0a157_b.jpg'),
-    new Recipe('A test recipe','This is simply a test maybe','https://c1.staticflickr.com/9/8409/30001106280_676f968089_b.jpg')
+    new Recipe('Dal tadka','This is simply a test maybe','https://c2.staticflickr.com/6/5134/30065852941_76d1d0a157_b.jpg'),
+    new Recipe('Gulab Jamun','This is simply a test maybe','https://c1.staticflickr.com/9/8409/30001106280_676f968089_b.jpg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
